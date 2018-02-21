f1bdb2cf9f23690bc441f225e1166ec7c36451b3
Rick te Molder <rtminnovations@gmail.com>
2018-02-21 15:47:52 +0100

    seeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+  name = Faker::Food.dish
+  address = Faker::Address.street_address
+  phone_number = Faker::PhoneNumber.phone_number
+  category = ["chinese", "italian", "japanese", "french", "belgian"].sample
+  res = Restaurant.create(name:name, address:address, phone_number:phone_number, category:category)
+
+  2.times do
+    content = Faker::HitchhikersGuideToTheGalaxy.quote
+    rating = rand(0..5)
+    Review.create(content: content, rating: rating, restaurant_id: res.id)
+  end
+end
+
+
+
